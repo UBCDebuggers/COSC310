eb7fcc46b3b3e3537fcd32457d19c290e5dea6e6
Unigwe <gabriel@gabitoprop.com>
2025-10-21 10:16:20 -0700

    pie

--- fullstack_project/backend/app/main.py
+++ fullstack_project/backend/app/main.py
@@ -1,3 +1,4 @@
+'''
 from fastapi import FastAPI
 from routers.items import router as items_router
 
@@ -8,3 +9,16 @@ def health():
     return {"status": "ok"}
 
 app.include_router(items_router)
+'''
+
+#use for mac
+#uvicorn fullstack_project.backend.app.main:app --reload 
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"ok": True, "msg": "Hello from FastAPI!"}
+

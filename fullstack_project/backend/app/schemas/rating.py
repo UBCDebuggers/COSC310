f742f7bb1e7f05fc7e07fb14bdc2be2811ec2b69
HakimRashid <rashidhakim514@gmail.com>
2025-10-23 09:42:21 -0700

    Resolved merge conflicts and removed unnecessary files.

--- fullstack_project/backend/app/schemas/rating.py
+++ fullstack_project/backend/app/schemas/rating.py
@@ -1,9 +1,9 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class Rating(BaseModel):
     id : str
     isbn : str
-    rating : str
+    rating : int = Field(min_length=1, max_length=5)
     
 class RatingCreate(BaseModel):
     id : str

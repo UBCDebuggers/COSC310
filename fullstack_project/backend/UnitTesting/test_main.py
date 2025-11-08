a9532aefac762f4b949a573a820120fbb688c8a6
HakimRashid <rashidhakim514@gmail.com>
2025-11-07 23:51:25 -0800

    Made search algorithm much faster. Added a global BOOKS variable to books_service that gets loaded on import. Sped up all books_service methods

--- fullstack_project/backend/UnitTesting/test_main.py
+++ fullstack_project/backend/UnitTesting/test_main.py
@@ -1,8 +1,10 @@
 from app.schemas.user import User, UserCreate
 from app.schemas.authentication import LoginRequest
 import pytest
+from app.repositories.books_repo import load_all
 from app.services.users_service import create_user, authenticate_user
-from app.services.books_service import search_by_char, search_by_string
+from app.services.books_service import filter
+from app.schemas.filter import Filter, DateRange
 from fastapi import HTTPException
 
 def test_create_user_success():
@@ -39,46 +41,79 @@ def test_authenticate_user():
     assert result.firstname == "john"
     assert result.lastname == "doe"
     
-def test_search_by_char():
-    test = "hello"
-    compend = "hello"
-    hits = search_by_char(compend, test)
-    
-    assert hits == 5
-    
-    test = "an"
-    compend = "banana"
-    hits = search_by_char(compend, test)
+# test filter by author
+def test_filter_author():
+    query = Filter(author="Kathleen E. Woodiwiss",
+                    publisher=None,
+                    publish_date_range= None)
+    books = [{'isbn': '0380816792', 'title': 'A Rose in Winter', 'author': 'Kathleen E. Woodiwiss', 'year_of_publication': '2011', 'publisher': 'Harper Mass Market Paperbacks'}, 
+             {'isbn': '068160204X', 'title': 'The Royals', 'author': 'Kitty Kelley', 'year_of_publication': '2020', 'publisher': 'Bausch & Lombard'}, 
+             {'isbn': '068107468X', 'title': 'Edgar Allen Poe Collected Poems', 'author': 'Edgar Allan Poe', 'year_of_publication': '2020', 'publisher': 'Bausch & Lombard'}, 
+             {'isbn': '0743457226', 'title': 'Deep Waters', 'author': 'Jayne Ann Krentz', 'year_of_publication': '2010', 'publisher': 'Pocket'}
+    ]
+    assert all("Kathleen E. Woodiwiss" == book.get('author') for book in filter(query, books))
 
-    assert hits == 5
+# test filter by publisher
+def test_filter_publisher():
+    query = Filter(author=None,
+                    publisher="Harper Mass Market Paperbacks",
+                    publish_date_range= None)
+    books = [{'isbn': '0380816792', 'title': 'A Rose in Winter', 'author': 'Kathleen E. Woodiwiss', 'year_of_publication': '2011', 'publisher': 'Harper Mass Market Paperbacks'}, 
+             {'isbn': '068160204X', 'title': 'The Royals', 'author': 'Kitty Kelley', 'year_of_publication': '2020', 'publisher': 'Bausch & Lombard'}, 
+             {'isbn': '068107468X', 'title': 'Edgar Allen Poe Collected Poems', 'author': 'Edgar Allan Poe', 'year_of_publication': '2020', 'publisher': 'Bausch & Lombard'}, 
+             {'isbn': '0743457226', 'title': 'Deep Waters', 'author': 'Jayne Ann Krentz', 'year_of_publication': '2010', 'publisher': 'Pocket'}
+    ]
+    assert all("Harper Mass Market Paperbacks" == book.get('publisher') for book in filter(query, books))
+
+# test filter by bounded date ranges
+def test_filter_date():
+    query = Filter(author=None,
+                    publisher=None,
+                    publish_date_range= DateRange(min=2019, max=2022))
+    books = [{'isbn': '0380816792', 'title': 'A Rose in Winter', 'author': 'Kathleen E. Woodiwiss', 'year_of_publication': '2011', 'publisher': 'Harper Mass Market Paperbacks'}, 
+             {'isbn': '068160204X', 'title': 'The Royals', 'author': 'Kitty Kelley', 'year_of_publication': '2020', 'publisher': 'Bausch & Lombard'}, 
+             {'isbn': '068107468X', 'title': 'Edgar Allen Poe Collected Poems', 'author': 'Edgar Allan Poe', 'year_of_publication': '2020', 'publisher': 'Bausch & Lombard'}, 
+             {'isbn': '0743457226', 'title': 'Deep Waters', 'author': 'Jayne Ann Krentz', 'year_of_publication': '2010', 'publisher': 'Pocket'}
+    ]
+    filtered_results = filter(query, books)
+    assert all(int(book.get('year_of_publication')) <= 2022 for book in filtered_results)
+    assert all(int(book.get('year_of_publication')) >= 2019 for book in filtered_results)
+
+# test filter by unbounded date ranges
+def test_filter_date_single():
+    query = Filter(author=None,
+                    publisher=None,
+                    publish_date_range= DateRange(min=None, max=2019))
+    books = [{'isbn': '0380816792', 'title': 'A Rose in Winter', 'author': 'Kathleen E. Woodiwiss', 'year_of_publication': '2011', 'publisher': 'Harper Mass Market Paperbacks'}, 
+             {'isbn': '068160204X', 'title': 'The Royals', 'author': 'Kitty Kelley', 'year_of_publication': '2020', 'publisher': 'Bausch & Lombard'}, 
+             {'isbn': '068107468X', 'title': 'Edgar Allen Poe Collected Poems', 'author': 'Edgar Allan Poe', 'year_of_publication': '2020', 'publisher': 'Bausch & Lombard'}, 
+             {'isbn': '0743457226', 'title': 'Deep Waters', 'author': 'Jayne Ann Krentz', 'year_of_publication': '2010', 'publisher': 'Pocket'}
+    ]
+    filtered_results = filter(query, books)
+    assert all(int(book.get('year_of_publication')) <= 2019 for book in filtered_results)
     
-def test_search_by_string():
-    test = "hello margeret"
-    compend = "hello margeret my name is hello, hello"
-    hits = search_by_string(compend, test)
+    query = Filter(author=None,
+                    publisher=None,
+                    publish_date_range= DateRange(min=2012, max= None))
+    books = [{'isbn': '0380816792', 'title': 'A Rose in Winter', 'author': 'Kathleen E. Woodiwiss', 'year_of_publication': '2011', 'publisher': 'Harper Mass Market Paperbacks'}, 
+             {'isbn': '068160204X', 'title': 'The Royals', 'author': 'Kitty Kelley', 'year_of_publication': '2020', 'publisher': 'Bausch & Lombard'}, 
+             {'isbn': '068107468X', 'title': 'Edgar Allen Poe Collected Poems', 'author': 'Edgar Allan Poe', 'year_of_publication': '2020', 'publisher': 'Bausch & Lombard'}, 
+             {'isbn': '0743457226', 'title': 'Deep Waters', 'author': 'Jayne Ann Krentz', 'year_of_publication': '2010', 'publisher': 'Pocket'}
+    ]
+    filtered_results = filter(query, books)
+    assert all(int(book.get('year_of_publication')) >= 2012 for book in filtered_results)
     
-    assert hits == 2
+ # test none filter
+def test_none_filter():
+    query = None
+    books = [{'isbn': '0380816792', 'title': 'A Rose in Winter', 'author': 'Kathleen E. Woodiwiss', 'year_of_publication': '2011', 'publisher': 'Harper Mass Market Paperbacks'}, 
+             {'isbn': '068160204X', 'title': 'The Royals', 'author': 'Kitty Kelley', 'year_of_publication': '2020', 'publisher': 'Bausch & Lombard'}, 
+             {'isbn': '068107468X', 'title': 'Edgar Allen Poe Collected Poems', 'author': 'Edgar Allan Poe', 'year_of_publication': '2020', 'publisher': 'Bausch & Lombard'}, 
+             {'isbn': '0743457226', 'title': 'Deep Waters', 'author': 'Jayne Ann Krentz', 'year_of_publication': '2010', 'publisher': 'Pocket'}
+    ]
+    filtered_results = filter(query, books)
     
-    test = "hello,margeret"
-    compend = "hello,margeret my name is hello, hello"
-    hits = search_by_string(compend, test)
-    
-    assert hits == 2
-    
-    test = "hello/margeret"
-    compend = "hello/margeret my name is hello, hello"
-    hits = search_by_string(compend, test)
-    
-    assert hits == 2
-    
-    test = "hello,margeret"
-    compend = "hello(margeret) my name is hello, hello"
-    hits = search_by_string(compend, test)
-    
-    assert hits == 2
-    
-    
-    
+    assert len(books) == len(filtered_results)
     
 if __name__ == "__main__":
-    pytest.main([__file__])
+    pytest.main([__file__]) 
--- fullstack_project/backend/app/services/books_service.py
+++ fullstack_project/backend/app/services/books_service.py
@@ -3,60 +3,66 @@ from collections import Counter
 from typing import List
 from fastapi import HTTPException
 from app.schemas.book import Book, BookCreate, BookUpdate
+from app.schemas.filter import Filter
 from app.repositories.books_repo import load_all, save_all
 
+BOOKS = load_all()
+    
 #Searches for a book by its title given a string. Returns the top 10 books that have the most matches by characters and words
-def search_books(tokens : str) -> List[Book]:
-    books = load_all()
+def search_books(tokens : str, filter_data : Filter) -> List[Book]:
     results_list = []
-    for book in books:
+    filtered_books = filter(filter_data, BOOKS)
+    
+    tokens = tokens.lower()
+    token_words = [w for w in re.split(r"[ .,/?;()]", tokens) if w]
+    token_word_set = set(token_words)
+    
+    for book in filtered_books:
         title = book.get('title')
-        
         title = title.lower()
-        tokens = tokens.lower()
+        title_word_set = set(title.split())
         
-        char_hits = search_by_char(title, tokens)
-        word_hits = search_by_string(title, tokens)
+        char_hits = sum(Counter(title)[ch] for ch in set(tokens) if ch in title)
+        word_hits = len(token_word_set & title_word_set)
         
         results_list.append({"char_hits" : char_hits, 
                              "word_hits": word_hits, 
-                             "isbn": book.get('isbn'), 
-                             "title" : title})
-        
+                             **book})
+    
     results_list = sorted( results_list, key=lambda item: (
         -item["word_hits"],  
         -item["char_hits"],  
         item["title"]        
         ))
     results_list = results_list[:10]
-    results_list = [get_book_by_isbn(book.get("isbn")) for book in results_list]
+    results_list = [Book(**{k: v for k, v in book.items() if k not in ("word_hits", "char_hits")}) for book in results_list]
     
     return results_list
-        
-#Checks for how many letters from each string match each other
-def search_by_char(string : str, compare : str) -> int:
-    total_hits = 0
-    string_counts = Counter(string)
-    for char_compare in set(compare):
-        if char_compare in string_counts:
-            total_hits += string_counts[char_compare]
-            
-    return total_hits
 
-#Checks for how many words from each string match a word in the other string
-def search_by_string(string : str, compare :str) -> int:
-    total_hits = 0
-    words = re.split(r"[ .,/?;()]", compare)
-    clean_words = [word for word in words if word]
-    for word in clean_words:
-        if word in string:
-            total_hits += 1
-    return total_hits
-            
+# Filters the book based on author, year of publication, publisher
+def filter(filter_data : Filter, books : List) -> List[Book]:
+    if filter_data is None:
+        return books
+    filter_author = filter_data.author.lower() if filter_data.author else None
+    filter_publisher = filter_data.publisher.lower() if filter_data.publisher else None
+    date_range = filter_data.publish_date_range
+
+    results = [
+        book for book in books
+        if ((filter_author is None or book.get("author").lower() == filter_author) 
+            and (filter_publisher is None or book.get('publisher').lower() == filter_publisher) 
+            and (date_range is None or (
+                (date_range.min is None or int(book["year_of_publication"]) >= date_range.min)
+                and (date_range.max is None or int(book["year_of_publication"]) <= date_range.max)
+            )
+            )
+        )
+    ]
+
+    return results
 
 def create_book(newBook: BookCreate) -> Book:
-    books = load_all()
-    if any(book.get("ISBN") == newBook.isbn for book in books):
+    if any(book.get("ISBN") == newBook.isbn for book in BOOKS):
         raise HTTPException(status_code=409, detail="ISBN collision; retry.")
     
     new_record = Book(isbn = newBook.isbn.strip(),
@@ -68,20 +74,18 @@ def create_book(newBook: BookCreate) -> Book:
                       img_url_m = newBook.img_url_m.strip(),
                       img_url_l = newBook.img_url_l.strip()
                       )
-    books.append(new_record.model_dump())
-    save_all(books)
+    BOOKS.append(new_record.model_dump())
+    save_all(BOOKS)
     return new_record
 
 def get_book_by_isbn(book_isbn: str) -> Book:
-    books = load_all()
-    for book in books:
+    for book in BOOKS:
         if book.get('isbn') == book_isbn:
             return Book(**book)
     raise HTTPException(status_code=404, detail=f"Book '{book_isbn}' not found")
 
 def update_book(book_isbn: str, bookUpdate : BookUpdate) -> Book:
-    books = load_all()
-    for id, book in enumerate(books):
+    for id, book in enumerate(BOOKS):
         if book.get("isbn") == book_isbn:
             updated = Book(isbn = bookUpdate.isbn.strip(),
                       title = bookUpdate.title.strip(),
@@ -92,15 +96,14 @@ def update_book(book_isbn: str, bookUpdate : BookUpdate) -> Book:
                       img_url_m = bookUpdate.img_url_m.strip(),
                       img_url_l = bookUpdate.img_url_l.strip()
                       )
-            books[id] = updated.model_dump()
-            save_all(books)
+            BOOKS[id] = updated.model_dump()
+            save_all(BOOKS)
             return updated
     raise HTTPException(status_code=404, detail=f"Book '{book_isbn}' not found")
 
 def delete_book(book_isbn: str) -> None:
-    books = load_all()
-    new_books = [book for book in books if book.get("isbn") != book_isbn]
-    if len(new_books) == len(books):
+    new_books = [book for book in BOOKS if book.get("isbn") != book_isbn]
+    if len(new_books) == len(BOOKS):
         HTTPException(status_code=404, detail=f"Book '{book_isbn}' not found")
     save_all(new_books)
         

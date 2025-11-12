3300634fdf3edf98f9c47dae4c68a48a23d82f33
Daniel Ogundare <dogun10517@gmail.com>
2025-11-11 19:30:05 -0800

    Added URL_To_Image feature in books_service.py

--- fullstack_project/backend/app/services/books_service.py
+++ fullstack_project/backend/app/services/books_service.py
@@ -1,3 +1,6 @@
+import requests
+from PIL import Image
+import io
 import re
 from collections import Counter
 from typing import List
@@ -106,6 +109,25 @@ def delete_book(book_isbn: str) -> None:
     if len(new_books) == len(BOOKS):
         HTTPException(status_code=404, detail=f"Book '{book_isbn}' not found")
     save_all(new_books)
-        
-            
+
+# Downloads and returns an image from a given URL
+def FindImage(x):
+	try:
+		response = requests.get(x)
+		response.raise_for_status()
+		image_data = io.BytesIO(response.content)
+		img = Image.open(image_data)
+
+		return img
+	except requests.exceptions.RequestException as e:
+		print(f"Error downloading the image: {e}")
+		return None
+	except IOError as e:
+		print(f"Error opening the image: {e}")
+		return None 
     
+    # If you want to write it out (aka return the image file):
+        # Make your variable (e.g. my_image = FindImage(url)
+        # Create an if statement of whether or not my_image has a file there or not (None)
+            # If so, then my_image.show())
+            # Else, print an error message

3a2bca0c2a9a32405a8d6f6dcf7987626159311e
HakimRashid <rashidhakim514@gmail.com>
2025-11-26 10:36:06 -0800

    Added penalties services along with unit tests for penalties repo and services.

--- fullstack_project/backend/app/schemas/penalties.py
+++ fullstack_project/backend/app/schemas/penalties.py
@@ -7,14 +7,14 @@ DEACTIVATED = 1
 LIMITED_ACTIONS = 2
 PERMANENT_BAN = 3
 
-class Pentalty(BaseModel):
+class Penalty(BaseModel):
     penalty_id : str = Field(default_factory= lambda: str(uuid.uuid4()))
     userid : str
     penalty_type : int = TEMPORARY_BAN
     description : str = "Visit your librarian for more information"
     timestamp : datetime = Field(default_factory= lambda: datetime.now(timezone.utc))
     expiry_date : datetime = Field(default_factory= lambda: datetime.now(timezone.utc) + timedelta(days=1))
-    active : bool
+    active : bool = True
     
 class PenaltyCreate(BaseModel):
     userid : str
